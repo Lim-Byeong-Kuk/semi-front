bf25b978bbf139af882d557ee81bdf83e936a5e6
FOREST <bye0414@naver.com>
2025-08-28 17:01:46 +0900

    제품 상세 페이지 뼈대 작성
    - DetailPage.js 작성
    - DetailComponent.js 작성
    - phonecaseData.js 를 phonecaseProducts.js 로 이름 수정
    - root.js 에 DetailPage 로 연결되는 라우팅 추가

---
+++ src/pages/phonecase/DetailPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DetailComponent from "../../components/phonecase/DetailComponent";
+
+const DetailPage = () => {
+  return (
+    <div>
+      DetailPage
+      <DetailComponent />
+    </div>
+  );
+};
+
+export default DetailPage;

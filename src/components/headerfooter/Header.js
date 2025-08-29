46fcd6989f31005d1a2161c36dc53cd8faed1cae
Shinsora <memo0754@naver.com>
2025-08-29 14:15:23 +0900

    headerfooter폴더 수정
    Navbar.js : 로고, 상단 오른쪽 유틸 메뉴를 Header.js로 분리,
    마이페이지와 장바구니 아이콘 추가
    Header.js : Navbar에서 로고, 유틸메뉴 코드 가져오고,
    로고 이미지 삭제, 이미지 대신 CASE 텍스트로 대체
    Footer.js : 텍스트 크기 수정

--- src/components/headerfooter/Header.js
+++ src/components/headerfooter/Header.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="w-full bg-white">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-5 py-3">
+        {/* 로고 텍스트 (클릭 시 '/' 경로로 이동) */}
+        <Link
+          to="/"
+          className="flex-shrink-0 text-3xl font-extrabold text-black cursor-pointer"
+        >
+          CASE
+        </Link>
+
+        {/* 상단 오른쪽 유틸 메뉴 */}
+        <nav className="flex gap-2 text-[10px] text-gray-500 font-medium">
+          <span className="cursor-pointer">로그인</span>
+          <span className="cursor-pointer">회원가입</span>
+          <span className="cursor-pointer">마이페이지</span>
+          <span className="cursor-pointer">주문조회</span>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/headerfooter/Navbar.js
+++ src/components/headerfooter/Navbar.js
@@ -1,65 +1,41 @@
 import React from "react";
+import Header from "./Header";
+import { FaUser, FaShoppingCart } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <header className="w-full bg-white">
-      {/* 상단 로고 + 우측 메뉴 */}
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-5 py-3">
-        {/* 로고 */}
-        <div className="flex-shrink-0">
-          <img
-            src="https://cdn.pixabay.com/photo/2025/07/11/20/21/beagle-9709437_1280.jpg"
-            alt="사이트 로고"
-            className="w-14 h-14 object-cover rounded-full border border-gray-300"
-          />
-        </div>
-
-        {/* 상단 오른쪽 유틸 메뉴 */}
-        <nav className="flex gap-2 text-[9px] text-gray-500 font-medium">
-          <span className="cursor-pointer hover:text-blue-600 transition">
-            로그인
-          </span>
-          <span className="cursor-pointer hover:text-blue-600 transition">
-            회원가입
-          </span>
-          <span className="cursor-pointer hover:text-blue-600 transition">
-            마이페이지
-          </span>
-          <span className="cursor-pointer hover:text-blue-600 transition">
-            주문조회
-          </span>
-        </nav>
-      </div>
-
-      {/* 메인 네비게이션 바 */}
-      <nav className="w-full bg-gray-50">
-        <div className="max-w-7xl mx-auto px-5">
-          <ul className="flex justify-center gap-7 overflow-x-auto whitespace-nowrap text-[9px] font-semibold text-gray-700 py-2">
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              방탄케이스
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              젤리케이스
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              카드케이스
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              하드케이스
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              Z플립 시리즈
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              에어팟케이스
-            </li>
-            <li className="cursor-pointer hover:text-blue-600 transition">
-              아크릴/스마트톡
-            </li>
+    <>
+      <Header />
+      <nav className="w-full bg-gray-50 sticky top-0 z-50 shadow-sm">
+        <div className="relative max-w-7xl mx-auto h-8 leading-8">
+          {/* 메뉴 리스트 */}
+          <ul className="absolute left-1/2 -translate-x-1/2 flex gap-7 whitespace-nowrap text-[10px] font-semibold text-gray-700">
+            <li className="cursor-pointer">방탄케이스</li>
+            <li className="cursor-pointer">젤리케이스</li>
+            <li className="cursor-pointer">카드케이스</li>
+            <li className="cursor-pointer">하드케이스</li>
+            <li className="cursor-pointer">Z플립 시리즈</li>
+            <li className="cursor-pointer">에어팟케이스</li>
+            <li className="cursor-pointer">아크릴/스마트톡</li>
           </ul>
+
+          {/* 아이콘 그룹 */}
+          <div
+            className="absolute top-0 bottom-0 flex items-center gap-4 text-gray-500 text-lg"
+            style={{ right: "48px" }}
+          >
+            <span className="cursor-pointer">
+              <FaUser title="마이페이지" />
+            </span>
+            <span className="cursor-pointer">
+              <FaShoppingCart title="장바구니" />
+            </span>
+          </div>
         </div>
       </nav>
-    </header>
+      임시 컨텐츠
+      <div style={{ height: "1500px", backgroundColor: "#f9f9f9" }} />
+    </>
   );
 };
 

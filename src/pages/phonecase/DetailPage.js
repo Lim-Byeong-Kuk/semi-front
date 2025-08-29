a4ce54703055d5161e7ed06351fb4681ef05d16c
FOREST <bye0414@naver.com>
2025-08-29 11:32:42 +0900

    푸터 적용 그리고 홈페이지 route 추가
    - Hompage, CartPage, ListPage, DetailPage 에 푸터적용
    - root.js 에 HomePage로 연결되는 path: "" 추가

--- src/pages/phonecase/DetailPage.js
+++ src/pages/phonecase/DetailPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import DetailComponent from "../../components/phonecase/DetailComponent";
+import Footer from "../../components/headerfooter/Footer";
 
 const DetailPage = () => {
   return (
     <div>
       DetailPage
       <DetailComponent />
+      <Footer />
     </div>
   );
 };
--- src/pages/phonecase/ListPage.js
+++ src/pages/phonecase/ListPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ListComponent from "../../components/phonecase/ListComponent";
+import Footer from "../../components/headerfooter/Footer";
 
 const ListPage = () => {
   return (
     <div>
       ListPage
       <ListComponent />
+      <Footer />
     </div>
   );
 };

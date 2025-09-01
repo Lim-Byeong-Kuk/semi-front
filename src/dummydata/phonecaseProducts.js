297898eeca9071e00aa591519d0f13ab0ae99e80
FOREST <bye0414@naver.com>
2025-09-01 11:12:32 +0900

    제품 더미데이터 추가
    - 폰케이스 제품 더미데이터를 id:100 까지 추가 함

--- src/dummydata/phonecaseProducts.js
+++ src/dummydata/phonecaseProducts.js
@@ -210,4 +210,477 @@ export const phonecaseProducts = [
     image:
       "https://akan.co.kr/upload/products/HARD/UIDH5003/thumb-single-graybg.webp",
   },
+  {
+    id: 31,
+    name: "나비로피어났어요 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5010/thumb-single.webp",
+  },
+  {
+    id: 32,
+    name: "데이지꽃2 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HJJS5031/thumb-single.webp",
+  },
+  {
+    id: 33,
+    name: "데이지꽃 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HMHS0009/thumb-single.webp",
+  },
+  {
+    id: 34,
+    name: "들꽃 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5040/thumb-single.webp",
+  },
+  {
+    id: 35,
+    name: "체리 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/AS0036/thumb-single.webp",
+  },
+  {
+    id: 36,
+    name: "꽃밭의풍경 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HIPS5044/thumb-single.webp",
+  },
+  {
+    id: 37,
+    name: "써니 식물 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/SS0036/thumb-single.webp",
+  },
+  {
+    id: 38,
+    name: "종종 데이지 커스텀 젤리케이스",
+    price: 7100,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/KYJS5038/thumb-single.webp",
+  },
+  {
+    id: 39,
+    name: "피치퍼플 커스텀 젤리케이스",
+    price: 7100,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0278/thumb-single.webp",
+  },
+  {
+    id: 40,
+    name: "프레임속으로 커스텀 젤리케이스",
+    price: 7100,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5092/thumb-single.webp",
+  },
+  {
+    id: 41,
+    name: "퍼플 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DDMS0005/thumb-single.webp",
+  },
+  {
+    id: 42,
+    name: "젖소무늬 커스텀 젤리케이스",
+    price: 7200,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0145/thumb-single.webp",
+  },
+  {
+    id: 43,
+    name: "쿨쿨잠 딸기꽃 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/CCJS5003/thumb-single.webp",
+  },
+  {
+    id: 44,
+    name: "써니 봄꽃 커스텀 젤리케이스",
+    price: 7300,
+    image: "https://akan.co.kr/upload/products/SOFTT/SS0053/thumb-single.webp",
+  },
+  {
+    id: 45,
+    name: "플로럴패턴 커스텀 젤리케이스",
+    price: 7300,
+    image: "https://akan.co.kr/upload/products/SOFTT/AS0023/thumb-single.webp",
+  },
+  {
+    id: 46,
+    name: "데이지 애그 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HMHS5020/thumb-single.webp",
+  },
+  {
+    id: 47,
+    name: "초록이 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5138/thumb-single.webp",
+  },
+  {
+    id: 48,
+    name: "색연필복숭아 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5048/thumb-single.webp",
+  },
+  {
+    id: 49,
+    name: "체리블라썸 커스텀 젤리케이스",
+    price: 7500,
+    image: "https://akan.co.kr/upload/products/SOFTT/AS0035/thumb-single.webp",
+  },
+  {
+    id: 50,
+    name: "three flowers 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0106/thumb-single.webp",
+  },
+  {
+    id: 51,
+    name: "FLOWER2 커스텀 젤리케이스",
+    price: 7400,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5154/thumb-single.webp",
+  },
+  {
+    id: 52,
+    name: "데이지3 커스텀 젤리케이스",
+    price: 7500,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5056/thumb-single.webp",
+  },
+  {
+    id: 53,
+    name: "튤립한가득 커스텀 젤리케이스",
+    price: 7500,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HMHS0006/thumb-single.webp",
+  },
+  {
+    id: 54,
+    name: "해바라기 커스텀 젤리케이스",
+    price: 8000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/ZSES5003/thumb-single.webp",
+  },
+  {
+    id: 55,
+    name: "여름바라기 커스텀 젤리케이스",
+    price: 7600,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/TITS5018/thumb-single.webp",
+  },
+  {
+    id: 56,
+    name: "듀플리시티 플라워 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DUPS6004/thumb-single.webp",
+  },
+  {
+    id: 57,
+    name: "봄한다발 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5064/thumb-single.webp",
+  },
+  {
+    id: 58,
+    name: "노란튤립 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/OHJS5009/thumb-single.webp",
+  },
+  {
+    id: 59,
+    name: "MAKOON love 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/MAKS5018/thumb-single.webp",
+  },
+  {
+    id: 60,
+    name: "레몬 커스텀 젤리케이스",
+    price: 8000,
+    image: "https://akan.co.kr/upload/products/SOFTT/ES0011/thumb-single.webp",
+  },
+  {
+    id: 61,
+    name: "아이스크림 커스텀 젤리케이스",
+    price: 7400,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0099/thumb-single.webp",
+  },
+  {
+    id: 62,
+    name: "데이지패턴 커스텀 젤리케이스",
+    price: 7800,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/KYJS5040/thumb-single.webp",
+  },
+  {
+    id: 63,
+    name: "말린튤립 커스텁 젤리케이스",
+    price: 7800,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/OHJS5018/thumb-single.webp",
+  },
+  {
+    id: 64,
+    name: "행복클로버 커스텀 젤리케이스",
+    price: 7300,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0230/thumb-single.webp",
+  },
+  {
+    id: 65,
+    name: "피치블러썸 커스텀 젤리케이스",
+    price: 7600,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/OHJS5051/thumb-single.webp",
+  },
+  {
+    id: 66,
+    name: "딸기 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5078/thumb-single.webp",
+  },
+  {
+    id: 67,
+    name: "봄꽃 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/ES0036/thumb-single.webp",
+  },
+  {
+    id: 68,
+    name: "쨀리공장 복숭아수박 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/GONS5026/thumb-single.webp",
+  },
+  {
+    id: 69,
+    name: "튤립드로잉 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/KYJS5013/thumb-single.webp",
+  },
+  {
+    id: 70,
+    name: "하트장미 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DDMS0002/thumb-single.webp",
+  },
+  {
+    id: 71,
+    name: "FLOWER1 커스텀 젤리케이스",
+    price: 7400,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5152/thumb-single.webp",
+  },
+  {
+    id: 72,
+    name: "행성2 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DONS5040/thumb-single.webp",
+  },
+  {
+    id: 73,
+    name: "낯선 정원 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/MINS5028/thumb-single.webp",
+  },
+  {
+    id: 74,
+    name: "데이지꽃 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HJJS5030/thumb-single.webp",
+  },
+  {
+    id: 75,
+    name: "썸머후르츠 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/YUIS5006/thumb-single.webp",
+  },
+  {
+    id: 76,
+    name: "파인애플 커스텀 젤리케이스",
+    price: 7300,
+    image: "https://akan.co.kr/upload/products/SOFTT/US5014/thumb-single.webp",
+  },
+  {
+    id: 77,
+    name: "피치핑크 커스텀 젤리케이스",
+    price: 7200,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0277/thumb-single.webp",
+  },
+  {
+    id: 78,
+    name: "드로잉장미 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/OHJS5041/thumb-single.webp",
+  },
+  {
+    id: 79,
+    name: "듀플리시티 페이즐리 커스텀 젤리케이스",
+    price: 7900,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DUPS6007/thumb-single.webp",
+  },
+  {
+    id: 80,
+    name: "레몬 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5047/thumb-single.webp",
+  },
+  {
+    id: 81,
+    name: "벚꽃 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5073/thumb-single.webp",
+  },
+  {
+    id: 82,
+    name: "그린 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DDMS5010/thumb-single.webp",
+  },
+  {
+    id: 83,
+    name: "피크닉 체크 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/MINS5035/thumb-single.webp",
+  },
+  {
+    id: 84,
+    name: "해바라기 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5061/thumb-single.webp",
+  },
+  {
+    id: 85,
+    name: "보랏빛꽃 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5008/thumb-single.webp",
+  },
+  {
+    id: 86,
+    name: "레몬 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/CBHS5017/thumb-single.webp",
+  },
+  {
+    id: 87,
+    name: "무궁화 커스텀 젤리케이스",
+    price: 7700,
+    image: "https://akan.co.kr/upload/products/SOFTT/ES0023/thumb-single.webp",
+  },
+  {
+    id: 88,
+    name: "데이지꽃패턴 커스텀 젤리케이스",
+    price: 7100,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/HMHS0008/thumb-single.webp",
+  },
+  {
+    id: 89,
+    name: "클로버 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/SJKS5036/thumb-single.webp",
+  },
+  {
+    id: 90,
+    name: "플로럴 그래니스퀘어 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/MINS5034/thumb-single.webp",
+  },
+  {
+    id: 91,
+    name: "빨간장미 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/OHJS5007/thumb-single.webp",
+  },
+  {
+    id: 92,
+    name: "꽃향기 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DONS5021/thumb-single.webp",
+  },
+  {
+    id: 93,
+    name: "화병과 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/SS0061/thumb-single.webp",
+  },
+  {
+    id: 94,
+    name: "나무열매 커스텀 젤리케이스",
+    price: 7100,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/SJKS5005/thumb-single.webp",
+  },
+  {
+    id: 95,
+    name: "벚꽃일러스트 커스텀 젤리케이스",
+    price: 7400,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/DDMS5009/thumb-single.webp",
+  },
+  {
+    id: 96,
+    name: "채소말장난02 커스텀 젤리케이스",
+    price: 7000,
+    image: "https://akan.co.kr/upload/products/SOFTT/KS0290/thumb-single.webp",
+  },
+  {
+    id: 97,
+    name: "몽글꽃패턴 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5079/thumb-single.webp",
+  },
+  {
+    id: 98,
+    name: "플로럴 선샤인 커스텀 젤리케이스",
+    price: 7300,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/MINS5027/thumb-single.webp",
+  },
+  {
+    id: 99,
+    name: "명자꽃패턴 커스텀 젤리케이스",
+    price: 7200,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/AHYS5043/thumb-single.webp",
+  },
+  {
+    id: 100,
+    name: "장미꽃다발2 커스텀 젤리케이스",
+    price: 7000,
+    image:
+      "https://akan.co.kr/upload/products/SOFTT/IUMS5060/thumb-single.webp",
+  },
 ];
